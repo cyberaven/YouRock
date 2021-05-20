9496cd64b5cc9b6c735ed840bcbcacdb4de9f8b3
cyberaven <cyberaven@yandex.ru>
2021-05-20 15:45:52 +0300

    Папки, камера.
    Папки, камера.

--- Assets/UI.cs
+++ Assets/UI.cs
@@ -1,11 +1,22 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class UI : MonoBehaviour
 {
+    [SerializeField] private GameObject selectLevelPanel;
     private Camera mainCamera;
     private Canvas canvas;
+
+    private void OnEnable()
+    {
+        StartLevelButton.StartLevelButtonEve += StartLevelButtonClk;
+    }
+    private void OnDisable()
+    {
+        StartLevelButton.StartLevelButtonEve -= StartLevelButtonClk;
+    }
 
     private void Awake()
     {
@@ -13,5 +24,10 @@ public class UI : MonoBehaviour
         mainCamera = Camera.main;
 
         canvas.worldCamera = mainCamera;
-    }    
+    }
+
+    private void StartLevelButtonClk(int id)
+    {
+        selectLevelPanel.SetActive(false);
+    }
 }
